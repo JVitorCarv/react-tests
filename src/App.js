90273b0746b77d4e6beb87aa3b8eda76cf4a71d9
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-01-31 20:07:05 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+//import Home from './components/pages/Home.js';
+//import Company from './components/pages/Company.js';
+//import Contact from './components/pages/Contact.js';
+//import NewProject from './components/pages/NewProject.js';
+
+//import Container from './components/layout/Container';
+
+function App() {
+  return (
+    <div>
+      <h1>Teste</h1>
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
